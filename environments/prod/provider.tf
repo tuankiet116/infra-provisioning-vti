32116514a0c35e58796b7defaf39cac9694ee3d7
tuankiet116 <kietdat1612000@gmail.com>
2025-09-08 02:26:54 +0700

    fix: Add kubernetes provider and cluster_ca_certificate output for PROD environment
    - Add kubernetes provider configuration in prod/provider.tf
    - Add cluster_ca_certificate output in eks module
    - Enable aws-auth ConfigMap deployment for PROD environment via CI/CD
    - Allows application team to use arn:aws:iam::234139188789:role/DE000079-prod-github-actions-deploy

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -10,7 +10,23 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 6.9.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20"
+    }
   }
 
   required_version = ">= 1.12.0"
+}
+
+# Kubernetes provider configuration for EKS
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "ap-southeast-2"]
+  }
 }
